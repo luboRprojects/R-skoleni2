2709d61cb13831205d50d583153682f1b9477c14
lubor <luboRprojects@users.noreply.github.com>
2014-05-13 20:47:40 +0200

    Update power_analysis.R

--- power_analysis.R
+++ power_analysis.R
@@ -2,4 +2,4 @@ library(pwr) #install.packages("pwr")
 effect.size <- (0.12-0.10)/0.06
 pwr.t.test(d = effect.size , sig.level = 0.05, 
 	      power = 0.8, type = c("two.sample"),
-		alternative="greater" )
+		alternative="greater" ) # parametr n= pro počet pozorování
